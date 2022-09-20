51ca84b778d74f01bb85d2269e72548656ea6847
kkeirstead <kkeirstead@microsoft.com>
2022-09-20 11:53:24 -0700

    Still experimenting with approaches

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 
 const main = async () => {
   try {
@@ -11,106 +12,125 @@ const main = async () => {
     const repo = core.getInput('repo', { required: true });
     const pr_number = core.getInput('pr_number', { required: true });
     const token = core.getInput('token', { required: true });
-
-    /**
-     * Now we need to create an instance of Octokit which will use to call
-     * GitHub's REST API endpoints.
-     * We will pass the token as an argument to the constructor. This token
-     * will be used to authenticate our requests.
-     * You can find all the information about how to use Octokit here:
-     * https://octokit.github.io/rest.js/v18
-     **/
-    const octokit = new github.getOctokit(token);
-
-    /**
-     * We need to fetch the list of files that were changes in the Pull Request
-     * and store them in a variable.
-     * We use octokit.paginate() to automatically loop over all the pages of the
-     * results.
-     * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-     */
-    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-      owner,
-      repo,
-      pull_number: pr_number,
-    });
+    const textToSearch = core.getInput('textToSearch', { required: true });
+    const textToAdd = core.getInput('textToAdd', { required: true });
+    const insertParameter = core.getInput('insertParameter', { required: false});
+    const paths = core.getInput('paths', {required: true});
 
 
-    /**
-     * Contains the sum of all the additions, deletions, and changes
-     * in all the files in the Pull Request.
-     **/
-    let diffData = {
-      additions: 0,
-      deletions: 0,
-      changes: 0
-    };
+    for (const path of paths.split(' ')) {
 
-    // Reference for how to use Array.reduce():
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    diffData = changedFiles.reduce((acc, file) => {
-      acc.additions += file.additions;
-      acc.deletions += file.deletions;
-      acc.changes += file.changes;
-      return acc;
-    }, diffData);
+        fs.readFile(path, (err, content) => {
+            if (err) throw err;
 
-    /**
-     * Loop over all the files changed in the PR and add labels according 
-     * to files types.
-     **/
-    for (const file of changedFiles) {
-      /**
-       * Add labels according to file types.
-       */
-      const fileExtension = file.filename.split('.').pop();
-      switch(fileExtension) {
-        case 'md':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['markdown'],
-          });
-        case 'js':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['javascript'],
-          });
-        case 'yml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml'],
-          });
-        case 'yaml':
-          await octokit.rest.issues.addLabels({
-            owner,
-            repo,
-            issue_number: pr_number,
-            labels: ['yaml'],
-          });
-      }
+            if (content.includes(textToSearch) == false)
+            {
+                if (textToAdd.includes("{insertFileName}"))
+                {
+                    textToAdd = textToAdd.replace("{insertFileName}", file.fileName.split('.')[0])
+                }
+                content += "\n" + textToAdd;
+
+                fs.writeFile(path, updatedContent);
+            }
+        });
     }
 
-    /**
-     * Create a comment on the PR with the information we compiled from the
-     * list of changed files.
-     */
-    await octokit.rest.issues.createComment({
-      owner,
-      repo,
-      issue_number: pr_number,
-      body: `
-        Pull Request #${pr_number} has been updated with: \n
-        - ${diffData.changes} changes \n
-        - ${diffData.additions} additions \n
-        - ${diffData.deletions} deletions \n
-      `
-    });
+    // /**
+    //  * Now we need to create an instance of Octokit which will use to call
+    //  * GitHub's REST API endpoints.
+    //  * We will pass the token as an argument to the constructor. This token
+    //  * will be used to authenticate our requests.
+    //  * You can find all the information about how to use Octokit here:
+    //  * https://octokit.github.io/rest.js/v18
+    //  **/
+    // const octokit = new github.getOctokit(token);
+
+    // /**
+    //  * We need to fetch the list of files that were changed in the Pull Request
+    //  * and store them in a variable.
+    //  * We use octokit.paginate() to automatically loop over all the pages of the
+    //  * results.
+    //  * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
+    //  */
+    // const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+    //   owner,
+    //   repo,
+    //   pull_number: pr_number,
+    // });
+
+
+    // /**
+    //  * Contains the sum of all the additions, deletions, and changes
+    //  * in all the files in the Pull Request.
+    //  **/
+    // let diffData = {
+    //   additions: 0,
+    //   deletions: 0,
+    //   changes: 0
+    // };
+
+    // // Reference for how to use Array.reduce():
+    // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
+    // diffData = changedFiles.reduce((acc, file) => {
+    //   acc.additions += file.additions;
+    //   acc.deletions += file.deletions;
+    //   acc.changes += file.changes;
+    //   return acc;
+    // }, diffData);
+
+    // /**
+    //  * Loop over all the files changed in the PR and add labels according 
+    //  * to files types.
+    //  **/
+    // for (const file of changedFiles) {
+    //     const { data: fileWithContents } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
+    //         owner: owner,
+    //         repo: repo,
+    //         path: file.filename.path
+    //     });
+
+    //     const fileExtension = file.filename.split('.').pop();
+    //     switch(fileExtension) {
+    //       case 'md':
+
+    //         var content = fileWithContents.content; // Parse this
+
+    //         if (content.includes(textToSearch) == false)
+    //         {
+    //             if (textToAdd.includes("{insertFileName}"))
+    //             {
+    //                 textToAdd = textToAdd.replace("{insertFileName}", file.fileName.split('.')[0])
+    //             }
+    //             content += "\n" + textToAdd;
+    //         }
+
+
+    //         /*
+    //         await octokit.rest.issues.addLabels({
+    //           owner,
+    //           repo,
+    //           issue_number: pr_number,
+    //           labels: ['markdown'],
+    //         });*/
+    //     }
+    // }
+
+    // /**
+    //  * Create a comment on the PR with the information we compiled from the
+    //  * list of changed files.
+    //  */
+    // await octokit.rest.issues.createComment({
+    //   owner,
+    //   repo,
+    //   issue_number: pr_number,
+    //   body: `
+    //     Pull Request #${pr_number} has been updated with: \n
+    //     - ${diffData.changes} changes \n
+    //     - ${diffData.additions} additions \n
+    //     - ${diffData.deletions} deletions \n
+    //   `
+    // });
 
   } catch (error) {
     core.setFailed(error.message);
