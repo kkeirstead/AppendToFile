a64883a002f32296e3af2db6320dadcad2501012
kkeirstead <kkeirstead@microsoft.com>
2022-09-26 14:17:21 -0700

    Removed comments; clean-up

--- index.js
+++ index.js
@@ -4,18 +4,12 @@ const fs = require('fs');
 
 const main = async () => {
   try {
-    /**
-     * We need to fetch all the inputs that were provided to our action
-     * and store them in variables for us to use.
-     **/
-    //const owner = core.getInput('owner', { required: true });
-    //const repo = core.getInput('repo', { required: true });
-    //const pr_number = core.getInput('pr_number', { required: true });
-    //const token = core.getInput('token', { required: true });
     const textToSearch = core.getInput('textToSearch', { required: true });
     const textToAdd = core.getInput('textToAdd', { required: true });
     const insertFileName = core.getInput('insertFileName', { required: false});
     const paths = core.getInput('paths', {required: false});
+
+    const insertFileNameParameter = "{insertFileName}";
 
     if (paths != "")
     {
@@ -27,29 +21,16 @@ const main = async () => {
                 if (content.includes(textToSearch) == false)
                 {
                     var updatedTextToAdd = textToAdd;
-                    if (insertFileName == 'true' && textToAdd.includes("{insertFileName}"))
+                    if (insertFileName == 'true' && textToAdd.includes(insertFileNameParameter))
                     {
                         var pathWithoutExtension = path.split('.')[0];
                         var fileName = pathWithoutExtension.split('/').pop();
-                        updatedTextToAdd = textToAdd.replace("{insertFileName}", fileName);
+                        updatedTextToAdd = textToAdd.replace(insertFileNameParameter, fileName);
                     }
-    
-                    //var contentStr = "abc";
     
                     var contentStr = content.toString();
     
-                    // var index = contentStr.indexOf("\n");
-                    // if (index !== -1) {
-                    //     contentStr = (contentStr.slice(0, index) + "" + contentStr.slice(index));
-                    // }
-    
-                    //contentStr = contentStr.substring(0, contentStr.length - 1) // Testing only
-    
                     contentStr = updatedTextToAdd + "\n\n" + contentStr;
-    
-                    //console.log(JSON.stringify(contentStr));
-    
-                    //contentStr = "  " + contentStr;
     
                     fs.writeFile(path, contentStr, (err) => {
     
@@ -58,103 +39,6 @@ const main = async () => {
             });
         }
     }
-
-    // /**
-    //  * Now we need to create an instance of Octokit which will use to call
-    //  * GitHub's REST API endpoints.
-    //  * We will pass the token as an argument to the constructor. This token
-    //  * will be used to authenticate our requests.
-    //  * You can find all the information about how to use Octokit here:
-    //  * https://octokit.github.io/rest.js/v18
-    //  **/
-    // const octokit = new github.getOctokit(token);
-
-    // /**
-    //  * We need to fetch the list of files that were changed in the Pull Request
-    //  * and store them in a variable.
-    //  * We use octokit.paginate() to automatically loop over all the pages of the
-    //  * results.
-    //  * Reference: https://octokit.github.io/rest.js/v18#pulls-list-files
-    //  */
-    // const { data: changedFiles } = await octokit.rest.pulls.listFiles({
-    //   owner,
-    //   repo,
-    //   pull_number: pr_number,
-    // });
-
-
-    // /**
-    //  * Contains the sum of all the additions, deletions, and changes
-    //  * in all the files in the Pull Request.
-    //  **/
-    // let diffData = {
-    //   additions: 0,
-    //   deletions: 0,
-    //   changes: 0
-    // };
-
-    // // Reference for how to use Array.reduce():
-    // // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-    // diffData = changedFiles.reduce((acc, file) => {
-    //   acc.additions += file.additions;
-    //   acc.deletions += file.deletions;
-    //   acc.changes += file.changes;
-    //   return acc;
-    // }, diffData);
-
-    // /**
-    //  * Loop over all the files changed in the PR and add labels according 
-    //  * to files types.
-    //  **/
-    // for (const file of changedFiles) {
-    //     const { data: fileWithContents } = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
-    //         owner: owner,
-    //         repo: repo,
-    //         path: file.filename.path
-    //     });
-
-    //     const fileExtension = file.filename.split('.').pop();
-    //     switch(fileExtension) {
-    //       case 'md':
-
-    //         var content = fileWithContents.content; // Parse this
-
-    //         if (content.includes(textToSearch) == false)
-    //         {
-    //             if (textToAdd.includes("{insertFileName}"))
-    //             {
-    //                 textToAdd = textToAdd.replace("{insertFileName}", file.fileName.split('.')[0])
-    //             }
-    //             content += "\n" + textToAdd;
-    //         }
-
-
-    //         /*
-    //         await octokit.rest.issues.addLabels({
-    //           owner,
-    //           repo,
-    //           issue_number: pr_number,
-    //           labels: ['markdown'],
-    //         });*/
-    //     }
-    // }
-
-    // /**
-    //  * Create a comment on the PR with the information we compiled from the
-    //  * list of changed files.
-    //  */
-    // await octokit.rest.issues.createComment({
-    //   owner,
-    //   repo,
-    //   issue_number: pr_number,
-    //   body: `
-    //     Pull Request #${pr_number} has been updated with: \n
-    //     - ${diffData.changes} changes \n
-    //     - ${diffData.additions} additions \n
-    //     - ${diffData.deletions} deletions \n
-    //   `
-    // });
-
   } catch (error) {
     core.setFailed(error.message);
   }
